ff8668c34a04532d5f64982ccf97b874641d0f4f
Stanislav Belichenko <stanislav.belichenko@x5.ru>
2025-03-18 17:56:37 +0300

    Пакет LLM стал верно работать с переменными окружения

--- cmd/llm/main.go
+++ cmd/llm/main.go
@@ -3,43 +3,46 @@ package llm
 import (
 	"context"
 	"fmt"
-	"github.com/joho/godotenv"
+	"github.com/ilyakaznacheev/cleanenv"
 	"github.com/sheeiavellie/go-yandexgpt"
 )
 
 const BotNickname = "Тринадцатый"
 
 type ConfigLLM struct {
-	llmApiToken    string `env:"LLM_API_TOKEN"`
-	systemPrompt   string
-	llmFolderId    string  `env:"LLM_FOLDER_ID"`
-	llmTemperature float32 `env:"LLM_TEMPERATURE" env-default:"0.7"`
-	maxTokens      int     `env:"LLM_MAX_TOKENS" env-default:"8000"`
+	LLMApiToken    string `env:"LLM_API_TOKEN"`
+	SystemPrompt   string
+	LLMFolderId    string  `env:"LLM_FOLDER_ID"`
+	LLMTemperature float32 `env:"LLM_TEMPERATURE" env-default:"0.7"`
+	MaxTokens      int     `env:"LLM_MAX_TOKENS" env-default:"8000"`
 }
 
-var config = ConfigLLM{}
+var config ConfigLLM
 
 func init() {
-	godotenv.Load()
-	config.systemPrompt = fmt.Sprintf(
+	err := cleanenv.ReadEnv(&config)
+	if err != nil {
+		fmt.Printf("Error reading LLM config: %v", err)
+	}
+	config.SystemPrompt = fmt.Sprintf(
 		"Тебя зовут %s. Ты очень полезный ИИ-помощник. Отвечай на вопросы коротко и точно. Если не знаешь ответ, вежливо напиши об этом. Не нужно уточнять, что отвечает именно менеджер ПИК.",
 		BotNickname,
 	)
 }
 
 func GetAnswerAboutKeys() (string, error) {
-	client := yandexgpt.NewYandexGPTClientWithAPIKey(config.llmApiToken)
+	client := yandexgpt.NewYandexGPTClientWithAPIKey(config.LLMApiToken)
 	request := yandexgpt.YandexGPTRequest{
-		ModelURI: yandexgpt.MakeModelURI(config.llmFolderId, yandexgpt.YandexGPT4Model),
+		ModelURI: yandexgpt.MakeModelURI(config.LLMFolderId, yandexgpt.YandexGPT4Model),
 		CompletionOptions: yandexgpt.YandexGPTCompletionOptions{
 			Stream:      false,
-			Temperature: config.llmTemperature,
-			MaxTokens:   config.maxTokens,
+			Temperature: config.LLMTemperature,
+			MaxTokens:   config.MaxTokens,
 		},
 		Messages: []yandexgpt.YandexGPTMessage{
 			{
 				Role: yandexgpt.YandexGPTMessageRoleSystem,
-				Text: config.systemPrompt,
+				Text: config.SystemPrompt,
 			},
 			{
 				Role: yandexgpt.YandexGPTMessageRoleUser,
