0e57d132dcbcf9d02736da86965684f7c7457ebf
Stanislav Belichenko <stanislav.belichenko@x5.ru>
2025-03-24 11:38:21 +0300

    Более корректная структура модуля

--- internal/handlers/command_handlers.go
+++ internal/handlers/command_handlers.go
@@ -2,7 +2,6 @@ package handlers
 
 import (
 	"fmt"
-	"github.com/ilyakaznacheev/cleanenv"
 	"s-belichenko/ilovaiskaya2-bot/cmd/llm"
 
 	tele "gopkg.in/telebot.v4"
@@ -24,42 +23,6 @@ var (
 
 	SetCommandsCommand = tele.Command{Text: "set_commands", Description: "Установить команды бота"}
 )
-
-type TeleContext interface {
-	Chat() *tele.Chat
-	Sender() *tele.User
-	Message() *tele.Message
-	Send(what interface{}, opts ...interface{}) error
-}
-
-type Config struct {
-	HouseChatId          int64  `env:"HOUSE_CHAT_ID"`          // Домовой чат, управляемый ботом
-	AdministrationChatID int64  `env:"ADMINISTRATION_CHAT_ID"` // Чат администраторов, куда поступают уведомления и тп
-	BotID                int64  // Собственный идентификатор бота
-	LogStreamName        string // Имя потока в YC Logs
-}
-
-// Общие переменные пакета
-var (
-	config = Config{LogStreamName: "main_stream"}
-	log    *yaLog.Logger
-)
-
-func init() {
-	initConfig()
-	log = yaLog.InitLog(config.LogStreamName)
-}
-
-func initConfig() {
-	err := cleanenv.ReadEnv(&config)
-	if err != nil {
-		fmt.Printf("Error reading Bot config: %v", err)
-	}
-}
-
-func SetBotID(botID int64) {
-	config.BotID = botID
-}
 
 func CommandHelpHandler(c tele.Context) error {
 	help := fmt.Sprintf(`
