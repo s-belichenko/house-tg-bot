e0c1de64c1c433378f6b4eba8a5ee931e93cc56f
Stanislav Belichenko <stanislav.belichenko@x5.ru>
2025-04-06 17:48:27 +0300

    Внедрение линтеров этап #6

---
+++ internal/handlers/helpers_internal_test.go
@@ -0,0 +1,100 @@
+package handlers
+
+import (
+	"testing"
+
+	"github.com/go-test/deep"
+	tele "gopkg.in/telebot.v4"
+)
+
+func TestParseUserID(t *testing.T) {
+	type ParseUserIDDataProvider struct {
+		testData map[string]string
+		expected map[string]int64
+	}
+
+	dpUserID := ParseUserIDDataProvider{
+		testData: map[string]string{
+			"Валидный user_id":      "123",
+			"Отрицательный user_id": "-123",
+			"Пустая строка":         "",
+			"Username с пробелом":   "123 ",
+		},
+		expected: map[string]int64{
+			"Валидный user_id":      123,
+			"Отрицательный user_id": 0,
+			"Пустая строка":         0,
+			"Username с пробелом":   0,
+		},
+	}
+
+	for testCase, data := range dpUserID.testData {
+		r := parseUserID(data)
+
+		for _, problem := range deep.Equal(r, dpUserID.expected[testCase]) {
+			t.Error(problem)
+		}
+	}
+}
+
+func TestParseUsername(t *testing.T) {
+	type ParseUsernameDataProvider struct {
+		testData map[string]string
+		expected map[string]string
+	}
+
+	dp := ParseUsernameDataProvider{
+		testData: map[string]string{
+			"Валидный username":            "username1",
+			"username с собачкой":          "@username1",
+			"Слишком короткий username":    "user",
+			"Минимально короткий username": "usern",
+			"Слишком длинный username": "@useruseruseruseruseruseruseruseruseruseruseruseruseruseruseruseru" +
+				"1useruseruseruseruseruseruseruseruseruseruseruseruseruseruseruseru1",
+			"Пустая строка":       "",
+			"Username с пробелом": "username2 ",
+		},
+		expected: map[string]string{
+			"Валидный username":            "username1",
+			"username с собачкой":          "",
+			"Слишком короткий username":    "",
+			"Минимально короткий username": "usern",
+			"Слишком длинный username":     "",
+			"Пустая строка":                "",
+			"Username с пробелом":          "",
+		},
+	}
+
+	for testCase, data := range dp.testData {
+		r := parseUsername(data)
+
+		for _, problem := range deep.Equal(r, dp.expected[testCase]) {
+			t.Error(problem)
+		}
+	}
+}
+
+func TestCreateUserViolator(t *testing.T) {
+	type CreateUserViolatorDataProvider struct {
+		testData map[string]string
+		expected map[string]*tele.User
+	}
+	dp := CreateUserViolatorDataProvider{
+		testData: map[string]string{
+			"Валидный user_id": "123",
+			"Пустой user_id":   "",
+		},
+		expected: map[string]*tele.User{
+			"Валидный user_id": {ID: 123},
+			"Пустой user_id":   nil,
+		},
+	}
+
+	for testCase, data := range dp.testData {
+		r := createUserViolator(data)
+
+		for _, problem := range deep.Equal(r, dp.expected[testCase]) {
+			t.Error(problem)
+		}
+	}
+}
