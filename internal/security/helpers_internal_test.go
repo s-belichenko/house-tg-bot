3b511391d8923aa33c3870672a1d450fab2bf8b1
Stanislav Belichenko <stanislav.belichenko@x5.ru>
2025-04-06 20:05:00 +0300

    Верное расположение моков пакета security

--- internal/security/helpers_internal_test.go
+++ internal/security/helpers_internal_test.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	tele "gopkg.in/telebot.v4"
-	mocks "s-belichenko/ilovaiskaya2-bot/mocks/internal_/handlers"
+	mocks "s-belichenko/ilovaiskaya2-bot/mocks/internal_/security"
 )
 
 func TestIsBotHouse(t *testing.T) {
